3737fb03fb44dcb7950a93e964955d9a7375d16c
william schultz <williamschultz97@gmail.com>
2026-02-23 13:19:05 +0100

    total pseudokode

---
+++ Kode til eksammensprojekt/algoritme.js
@@ -0,0 +1,17 @@
+
+let korridorLokation = [[1,1,0],[2,1,0]];//(start(x,y,z), slut(x,y,z))
+let korridorArray=[[1,korridorLokation," nord 0.sal"],[2,korridorLokation,"syd 0.sal"]];//(korridorId, korridorLokation, navn)
+
+
+let dørLokation1 = [1,2,0];//(x,y,z)
+let dørLokation2 = [2,2,0];//(x,y,z)
+let dørArray = [[1,dørLokation1,1],[1,dørLokation2,2]];//(dørid,dørLokation, lokaleTilknyttet 
+let userDøre = [1,2] //(dørId1, dørId2);
+
+function algoritme (dørArray,userDøre,korridorArray){
+
+
+
+
+
+}
