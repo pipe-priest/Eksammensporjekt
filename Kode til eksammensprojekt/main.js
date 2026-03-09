e4ecdfcd43d882779443422baa2aab061750c066
emil moller <emil.vestergaard.moller@gmail.com>
2026-03-09 14:27:10 +0100

    Refactor code structure for improved readability and maintainability

---
+++ Kode til eksammensprojekt/main.js
@@ -0,0 +1,8 @@
+const startInput = document.getElementById("start"); // Hent input-elementet
+const startnr = startInput.value;           // Det er en streng
+const start = parseFloat(startnr);          // Konverter til et nummer
+
+const slutInput = document.getElementById("slut"); // Hent input-elementet
+const slutnr = slutInput.value;           // Det er en streng
+const slut = parseFloat(slutnr);          // Konverter til et nummer
+
